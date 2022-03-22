2ce6ccdb56a77d6b25412e82f66df1c932b08d35
grboehringer <g2boehringer@gmail.com>
2022-03-21 19:06:11 -0500

    first commit

---
+++ thingy.py
@@ -0,0 +1,16 @@
+import asyncio
+from bleak import BleakScanner
+from bluepy.thingy52 import Thingy52
+
+async def scan():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        if d.name == 'Fidget':
+            print(d.name)
+            print(d.details)
+            print(d.metadata)
+            print(d.rssi)
+            return d.address
+
+address = asyncio.run(scan())
+thingy = Thingy52(address)
